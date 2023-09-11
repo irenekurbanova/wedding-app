741567a03e1d90e1a6d6529821b61d1595b96299
irenekurbanova <kirina2504@gmail.com>
2023-09-11 13:36:12 +0300

    edit btn flexcontainer

--- src/components/UI/ConfirmationButton.js
+++ src/components/UI/ConfirmationButton.js
@@ -2,16 +2,14 @@ import styles from "./ConfirmationButton.module.css";
 
 const ConfirmationButtonButton = (props) => {
   return (
-    <div className={styles.init}>
-      <div className={styles.wrapper}>
-        <button
-          type={props.type}
-          className={styles.button}
-          onClick={props.onClick}
-        >
-          <span>{props.children}</span>
-        </button>
-      </div>
+    <div className={styles.wrapper}>
+      <button
+        type={props.type}
+        className={styles.button}
+        onClick={props.onClick}
+      >
+        <span>{props.children}</span>
+      </button>
     </div>
   );
 };
